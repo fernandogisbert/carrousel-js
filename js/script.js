85e396e4d9a397eaf480748bf44d3d3ca5bafeed
LAPTOP-1U1SBC38\ferna <ferbert15@gmail.com>
2021-02-02 19:00:43 +0100

    radiobuttons implementados

--- js/script.js
+++ js/script.js
@@ -4,21 +4,26 @@ const objetoImg = document.querySelector('#imagen');
 const botonAvanzar = document.querySelector('#avanzar');
 const botonRetroceder = document.querySelector('#retroceder');
 
+const templateCirculos = document.querySelector('#template-circulo').content.firstElementChild;
+const circulos = document.querySelector('#circulos');
+
 const imagenes = ['img/foto1.jpg', 'img/foto2.jpg', 'img/foto3.jpg'];
 let pagina = 1;
 
 
 //funciones 
+function cambiarPagina(nuevaPagina) {
+    pagina = nuevaPagina;
+    render();
+}
 
 function avanzarFoto() {
     pagina = pagina + 1;
 
     //verifico que no he alcanzado el limite y le pongo el valor a pagina
-
     if(imagenes.length + 1  <= pagina ) {
         pagina = 1
     }
-
     render();
 }
 
@@ -34,7 +39,28 @@ function retrocederFoto() {
 }
 
 function render() {
-    objetoImg.setAttribute('src', imagenes[pagina - 1])
+    //imagenes
+    objetoImg.setAttribute('src', imagenes[pagina - 1]);
+    //circulos
+
+    circulos.textContent = '';
+
+    imagenes.forEach(function(imagen, indice){
+
+        const nuevoCirculo = templateCirculos.cloneNode(true);
+
+        nuevoCirculo.addEventListener('click', function() {
+            cambiarPagina(indice + 1)
+        });
+
+        //mostramos el color relleno en el que este
+        if(pagina === (indice + 1)) {
+            nuevoCirculo.setAttribute('checked', true);
+        }
+        
+        //mostramos
+        circulos.appendChild(nuevoCirculo);
+    })
 }
 
 //eventos
